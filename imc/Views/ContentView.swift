b44c3d19ffd68fc9bdabe26620fbc6840ed6297f
felipesantos22 <felipevs2018@gmail.com>
2024-12-11 14:01:00 -0300

    insert icon in the home

--- imc/Views/ContentView.swift
+++ imc/Views/ContentView.swift
@@ -11,7 +11,6 @@ import SwiftData
 struct ContentView: View {
     var body: some View {
         ImcView()
-//        ImcTableView()
     }
 }
 
--- imc/Views/ImcView.swift
+++ imc/Views/ImcView.swift
@@ -13,40 +13,45 @@ struct ImcView: View {
     @StateObject private var viewModel = ImcViewModel()
     
     var body: some View {
-        VStack {
-            Text("Cálculo de IMC")
-                .font(.title)
-                .foregroundStyle(.black)
-            Text("Peso")
-                .font(.headline)
-                .padding(.top, 10)
-            TextField("Peso", text: $viewModel.weightValue)
-                .keyboardType(.decimalPad)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal)
-            
-            Text("Altura")
-                .font(.headline)
-                .padding(.top, 10)
-            TextField("Altura", text: $viewModel.heightValue)
-                .keyboardType(.decimalPad)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal)
-            
-            Button(action: {
-                viewModel.calculateIMC()  // Chama a função corretamente sem o $
-            }) {
-                Text("Calcular IMC")
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(Color.black)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-                    .padding(.top, 20)
+        ZStack {
+            VStack {
+                Label("IMC", systemImage: "person.crop.circle")
+                    .font(.title)
+                    .foregroundStyle(.black)
+                    .padding(.top, 10)
+                Text("Peso")
+                    .font(.system(size: 22))
+                    .padding(.top, 10)
+                TextField("Peso", text: $viewModel.weightValue)
+                    .keyboardType(.decimalPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal)
+                
+                Text("Altura")
+                    .font(.system(size: 22))
+                    .padding(.top, 10)
+                TextField("Altura", text: $viewModel.heightValue)
+                    .keyboardType(.decimalPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal)
+                
+                Button(action: {
+                    viewModel.calculateIMC()  // Chama a função corretamente sem o $
+                }) {
+                    Text("Calcular IMC")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.black)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .padding(.top, 20)
+                }
+                .padding(10)
+                Text("IMC: \(viewModel.imcResult)").bold()// Use a variável diretamente
             }
-            .padding(10)
-            Text("IMC: \(viewModel.imcResult)") // Use a variável diretamente
         }
         .padding()
     }
 }
+
+
